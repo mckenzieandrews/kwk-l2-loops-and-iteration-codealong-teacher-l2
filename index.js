98417d158d87b18604b127d5cedf923cd745fddd
McKenzie Andrews <c.mckenzie.a@gmail.com>
2018-08-09 17:52:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 // Code your solutions in this file
-function printBadges(names) {
+function printBadges (names) {
   for (let i = 0; i < names.length; i++) {
-    console.log(`Welcome ${names[i]}! You are employee #${i + 1}.`)
+    console.log(`Welcome ${names[i]}! You are employee #${i + 1}.`);
   }
+
   return names;
 }
 
-function tailsNeverFails() {
-  let counter = 0;
-  while(Math.round(Math.random()) === 0) {
-    coin = Math.floor(Math.random());
-    counter = counter + 1;
-  }
-  console.log(`You got ${counter} tails in a row!`)
-}
+function tailsNeverFails () {
+  let tails = 0;
 
-printBadges(['Joe', 'Gabe']);
+  while (Math.random() >= 0.5) {
+    tails++;
+  }
+
+  return `You got ${tails} tails in a row!`;
+}
